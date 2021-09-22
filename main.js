a3ab338aeb5dcd5cd60214e99d0ce65df80cb8a3
Salonee-Jain <salonijain1248@gmail.com>
2021-09-22 23:20:46 +0530

    added the JavaScript file⚙

---
+++ main.js
@@ -0,0 +1,15 @@
+function ageInDays(){
+    let year = document.getElementById('age').value;
+    let days = (2021-year)*365;
+    var element = document.getElementById("result");
+    var h1 = document.createElement("h1");
+    var text = document.createTextNode("You are "+days+" days old:)");
+    h1.setAttribute('id', 'ageInDays')
+    h1.appendChild(text)
+    element.appendChild(h1);
+}
+
+function RESet(){
+    document.getElementById('ageInDays').remove();
+    document.getElementById('age').value = '';
+}
